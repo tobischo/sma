050672c89cd636c3e379c8e4b21fae29e7fa43da
Tobias Schoknecht <tobias.schoknecht@gmail.com>
2011-11-18 19:08:17 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanmgmtappl::Application.config.secret_token = 'cf8f2d460279d27cecc13f67050c8c125b51518cde4b3345d1d098535b4c8597991ba106f83690a18895f79103cbea83cefe332e634c32f8a6e8cc48095c060e'
